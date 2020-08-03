76d7f138a37209b55dd4c611456471e30f6b96ae
Max Miao <miaoma2@oocl.com>
2020-08-03 20:27:34 +0800

    [react-demo][max]feature: pass add and reduce number

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -4,19 +4,33 @@ class Counter extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = { value: 0 }
+        this.state = { 
+            value: 0,
+            groupSize: 0 
+        }
     }
 
     addNumber = () => {
         this.setState((prevState)=>({
             value: prevState.value + 1
         }))
+        this.props.onIncrease();
     }
 
     reduceNumber = () => {
         this.setState((prevState)=>({
             value: prevState.value - 1
         }))
+        this.props.onReduce();
+    }
+
+    static getDerivedStateFromProps(props,state){
+        if(props.groupsize!=state.groupsize){
+            return {
+                value:0,
+                groupsize:props.groupsize
+            };
+        }
     }
 
     render() {
--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -4,23 +4,49 @@ import Counter from '../Counter';
 class CountGroup extends Component{
     constructor(props){
         super(props);
-        this.state = {size: props.size}
+        this.state = {
+            size: props.size,
+            totalNumber: 0,
+        };
     }
 
-    onResize = (event) =>{
+    handleResize = (event) =>{
+        if(event.target.value==this.state.size){
+            return null;
+        }
         this.setState({
-            size: event.target.value ? parseInt(event.target.value) : 0
+            size: event.target.value ? parseInt(event.target.value) : 0,
+            totalNumber: 0
         })
+    }
+
+    handleIncrease = () =>{
+        this.setState((prevState)=>({
+            totalNumber: prevState.totalNumber + 1
+        }))
+    }
+    
+    handleReduce = () =>{
+        this.setState((prevState)=>({
+            totalNumber: prevState.totalNumber - 1
+        }))
     }
 
     render(){
         const initArray = [...Array(this.state.size).keys()];
-        console.log(initArray)
+        // console.log(this.state.countList)
         return <div>
-            <label>Group size: </label>
-            <input onBlur={this.onResize} defaultValue = {6}/>
+            <div>
+                <label>Group size: </label>
+                <input onBlur={this.handleResize} defaultValue = {6}/>
+            </div>
+            <div>
+                <label>Total Number: </label>
+                <span>{this.state.totalNumber}</span>
+            </div>
             {
-                initArray.map(key => <Counter key={key}/>)
+                initArray.map(key => 
+                <Counter groupsize={this.state.size} onIncrease={this.handleIncrease} onReduce={this.handleReduce}  key={key}/>)
             }
         </div>
     }
